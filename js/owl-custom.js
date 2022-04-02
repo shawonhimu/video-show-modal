b3ec90ce4772fae1de89ddff9d818d8229d6250f
Shawon Himu <83220594+shawonhimu@users.noreply.github.com>
2022-04-02 10:38:57 +0600

    video-modal-v1.00

---
+++ js/owl-custom.js
@@ -0,0 +1,28 @@
+
+
+
+$('#owlHome01').owlCarousel({
+    
+    rtl:false,
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3500,
+    autoplayHoverPause:true,
+    dots:true,
+    rewindNav :true,
+    arrows:true,
+    ltr:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        664:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
