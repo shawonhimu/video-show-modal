f4d3d5262c94e60f4d9300c29e59b89934b82ed0
Shawon Himu <83220594+shawonhimu@users.noreply.github.com>
2022-04-02 11:38:22 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 
+var youTubeVideoID='QZnVRvTJzcU';
+
+var youTubeURL="https://www.youtube.com/embed/"+youTubeVideoID;
+
+var videoModal = document.querySelector('#modalid');
+var iframeModal = document.querySelector('iframe');
+videoModal.addEventListener('hide.bs.modal', function () {
+    iframeModal.setAttribute('src','');
+})
+videoModal.addEventListener('show.bs.modal', function () {
+    iframeModal.setAttribute('src', youTubeURL);
+})
